9be9af74dca037d32a5d8d4829568812ef4cfbec
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-09-15 05:04:32 +0545

    comparision operators

--- SQL/basics.sql
+++ SQL/basics.sql
@@ -9,3 +9,18 @@ SELECT * FROM users;
 
 -- applying filter using where 
 SELECT * FROM users where age > 12;
+
+-- Comparision operator 
+SELECT * FROM users where age =12
+
+SELECT * FROM users WHERE age != 21;
+
+-- AND OPERATOR
+SELECT * from users WHERE age>21 AND age<30;
+
+-- OR OPERATOR
+SELECT * FROM users where age = 21 OR age = 25
+
+-- NOT Operator 
+SELECT * FROM users WHERE NOT age = 21;
+WHERE age! = 21;
