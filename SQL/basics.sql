052b07fa2d06bf6ff77d3b83c682efc37d4ce1b0
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-09-15 11:11:37 +0545

    MySQL basic operators/functions

--- SQL/basics.sql
+++ SQL/basics.sql
@@ -32,3 +32,40 @@ SELECT * FROM users WHERE status IN ('pending','in-progress')
 
 -- BETWEEN operator in MySQL 
 SELECT * FROM users where age BETWEEN 20 and 30;
+
+-- LIKE OPERATOR IN MySQL 
+SELECT * FROM users WHERE name LIKE 'ra%'
+
+SELECT * FROM users WHERE name LIKE '%am'
+
+SELECT * FROM USERS WHERE name LIKE '%ram%'
+
+
+-- NULL operator in MySQL
+WHERE email IS NULL; --> CORRECT 
+WHERE email = NULL --> WRONG 
+
+WHERE email IS NOT NULL
+
+-- ORDER BY FUNCTION 
+SELECT * FROM users ORDER BY age DESC;
+SELECT * FROM users ORDER BY age ASC;
+SELECT * FROM users ORDER BY name ASC;
+
+-- LIMIT In MySQL
+SELECT * FROM users LIMIT 20;
+SELECT * FROM users LIMIT 10;
+
+-- OFFSET in MySQL 
+SELECT * FROM users LIMIT 10 OFFSET 20;
+
+SELECT id, title, status FROM tasks ORDER BY created_at DESC LIMIT 10 OFFSET 20;
+
+-- Distinct Functions 
+SELECT DISTINCT status FROM tasks;
+-- gives -> pending completed 
+
+
+-- ALIASES- AS 
+SELECT name AS username FROM users; // --> you get username in result instead of name 
+SELECT u.name FROM users AS u;
